c935eb634751901558db1f8c03902f67a7741055
ahuearr <ahuearr@gmail.com>
2015-07-31 22:48:52 +0200

    Issue # Mejorar navegacion entre pantallas

--- app/src/main/java/com/gusycorp/travel/activity/TripTransportActivityOK.java
+++ app/src/main/java/com/gusycorp/travel/activity/TripTransportActivityOK.java
@@ -1,6 +1,9 @@
 package com.gusycorp.travel.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.gusycorp.travel.R;
@@ -14,10 +17,31 @@ import java.util.List;
 
 public class TripTransportActivityOK extends MenuActivity {
 
+	Button back;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_transport_trip_ok);
 
+		back = (Button) findViewById(R.id.back_button);
+		back.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				goTripTransportList();
+			}
+		});
+	}
+
+	@Override
+	public void onBackPressed() {
+		goTripTransportList();
+	}
+
+	private void goTripTransportList(){
+		Intent intent = new Intent(this, TripTransportListActivity.class);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		intent.putExtra("tripName", tripName);
+		startActivity(intent);
+		finish();
 	}
 }
