ba86561cd880fd9545b3645ac604ba270bebe850
ahuearr <ahuearr@gmail.com>
2015-08-19 18:16:23 +0200

    Issue #10 Mostrar en mapa alojamientos por coordenada o direccion

--- app/src/main/java/com/gusycorp/travel/activity/TripMapActivity.java
+++ app/src/main/java/com/gusycorp/travel/activity/TripMapActivity.java
@@ -1,7 +1,12 @@
 package com.gusycorp.travel.activity;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -19,8 +24,20 @@ import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseRelation;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class TripMapActivity extends FragmentActivity {
 
@@ -143,11 +160,256 @@ public class TripMapActivity extends FragmentActivity {
             Double latitude = item.getLatitude();
             Double longitude = item.getLongtiude();
             if(latitude!=null && latitude!=0.0 && longitude!=null && longitude!=0.0){
-                mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(getString(R.string.transportDepartureFrom) + " " + item.getFrom()));
+                mMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(item.getPlace()+" "+item.getAddress()+" "+item.getCity()));
             } else {
-
+                if(!"".equals(item.getPlace()) || !"".equals(item.getAddress()) || !"".equals(item.getCity())){
+                    String place = item.getPlace() + " " + item.getAddress() + " " + item.getCity();
+                    Address address = getAddressFromString(place);
+                    if (address != null) {
+                        LatLng placePosition = new LatLng(address.getLatitude(),
+                                address.getLongitude());
+                        mMap.addMarker(new MarkerOptions().position(placePosition).title(place));
+                    } else {
+                        new GetLocationTask(place).execute();
+                    }
+                }
             }
         }
 
     }
+
+    private Address getAddressFromString(final String location) {
+        Address result = null;
+        List<Address> addresses = null;
+        if (location != null) {
+            try {
+                Geocoder geocoder = new Geocoder(this);
+                addresses = geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (addresses != null && addresses.size() > 0) {
+                result = addresses.get(0);
+            }
+        }
+        return result;
+    }
+
+    private class GetLocationTask extends AsyncTask<Void, Void, Address> {
+        JSONObject jsonObject;
+        String addressString;
+
+        public GetLocationTask(String locationName) {
+            this.addressString = locationName;
+        }
+
+        @Override
+        protected Address doInBackground(Void... params) {
+            return getLocationInfo(addressString);
+        }
+
+        private Address getLocationInfo(String address) {
+
+            String query = "http://maps.google.com/maps/api/geocode/json?address="
+                    + address.replaceAll("  ", " ").replaceAll(" ", "%20")
+                    + "&sensor=false";
+            Log.e("TAG", query);
+            Address addr = null;
+            HttpClient client = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(query);
+
+            HttpResponse response;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            try {
+                response = client.execute(httpGet);
+                if (response.getStatusLine().getStatusCode() == 200) {
+
+                    HttpEntity entity = response.getEntity();
+                    InputStream stream = entity.getContent();
+
+                    int b;
+                    while ((b = stream.read()) != -1) {
+                        stringBuilder.append((char) b);
+                    }
+                    try {
+                        jsonObject = new JSONObject(stringBuilder.toString());
+                        Log.e("TAG", jsonObject.get("status").toString());
+                        if ("OK".equals(jsonObject.get("status").toString())) {
+                            addr = new Address(Locale.getDefault());
+
+                            Double lon = null;
+                            Double lat = null;
+
+                            Log.e("TAG", jsonObject.get("results").toString());
+                            JSONObject geometryJsonObject = ((JSONArray) jsonObject
+                                    .get("results")).getJSONObject(0)
+                                    .getJSONObject("geometry");
+                            // Se comprueba si hay geometria y si hay geometria
+                            // si hay coordenadas y se crea el objecto Address
+                            // con los parametros recibidos del geocoder
+                            if (geometryJsonObject != null) {
+                                JSONObject locationJsonObject = geometryJsonObject
+                                        .getJSONObject("location");
+                                if (locationJsonObject != null) {
+                                    lon = locationJsonObject.getDouble("lng");
+                                    lat = locationJsonObject.getDouble("lat");
+
+                                    if (lon != null && lat != null) {
+                                        addr.setLatitude(lat);
+                                        addr.setLongitude(lon);
+
+                                        JSONArray addrComp = ((JSONArray) jsonObject
+                                                .get("results")).getJSONObject(
+                                                0).getJSONArray(
+                                                "address_components");
+
+                                        for (int i = 0; i < addrComp.length(); i++) {
+                                            JSONObject addressComponentJsonObject = addrComp
+                                                    .getJSONObject(i);
+                                            JSONArray typeJsonArray = addressComponentJsonObject
+                                                    .getJSONArray("types");
+                                            for (int j = 0; j < typeJsonArray
+                                                    .length(); j++) {
+                                                if ("route"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Es la calle (No seteable)
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                } else if ("neighborhood"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Puede ser la calle. (No
+                                                    // seteable)
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                } else if ("locality"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Es la poblacion
+                                                    addr.setLocality(addressComponentJsonObject
+                                                            .getString("long_name"));
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                } else if ("administrative_area_level_2"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Es la provincia
+                                                    addr.setAdminArea(addressComponentJsonObject
+                                                            .getString("long_name"));
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                } else if ("administrative_area_level_1"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Es la CCAA (No seteable
+                                                    // si hay provincia)
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                } else if ("country"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Es el Pais
+                                                    addr.setCountryName(addressComponentJsonObject
+                                                            .getString("long_name"));
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                } else if ("street_number"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Es la numero de calle (No
+                                                    // seteable)
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                } else if ("postal_code"
+                                                        .equals(typeJsonArray
+                                                                .getString(j))) {
+                                                    // Es el Codigo Postal
+                                                    addr.setPostalCode(addressComponentJsonObject
+                                                            .getString("long_name"));
+                                                    break; // Se rompe el bucle
+                                                    // porque type puede
+                                                    // tener
+                                                    // varios valores y
+                                                    // si hemos
+                                                    // encontrado 1 el
+                                                    // resto no importa
+                                                }
+                                            }
+                                        }
+
+                                    } else {
+                                        addr = null;
+                                    }
+                                } else {
+                                    addr = null;
+                                }
+                            } else {
+                                addr = null;
+                            }
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return addr;
+
+        }
+
+        @Override
+        protected void onPostExecute(Address address) {
+            if (address != null) {
+
+                LatLng placePosition = new LatLng(
+                        address.getLatitude(), address.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(placePosition).title(addressString));
+
+            } else {
+                Toast.makeText(TripMapActivity.this,
+                        "La direccion no se encuentra en el mapa",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
