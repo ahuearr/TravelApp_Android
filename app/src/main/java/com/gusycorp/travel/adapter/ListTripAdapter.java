505e2bba5e7705a8e975097aace524344c40241b
ahuearr <ahuearr@gmail.com>
2015-09-03 00:32:19 +0200

    Issue #13 Distinguir en los viajes los organizados de los participados

--- app/src/main/java/com/gusycorp/travel/adapter/ListTripAdapter.java
+++ app/src/main/java/com/gusycorp/travel/adapter/ListTripAdapter.java
@@ -9,11 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gusycorp.travel.R;
 import com.gusycorp.travel.model.Trip;
 import com.gusycorp.travel.util.Constants;
+import com.parse.ParseUser;
 
 public class ListTripAdapter extends ArrayAdapter<Trip> {
 
@@ -82,6 +84,7 @@ public class ListTripAdapter extends ArrayAdapter<Trip> {
 				convertView = mInflater.inflate(R.layout.row_list_trip, null);
 				holder.textView = (TextView) convertView
 						.findViewById(R.id.text);
+				holder.roleView = (ImageView) convertView.findViewById(R.id.trip_role);
 				break;
 			case TYPE_SEPARATOR:
 				convertView = mInflater
@@ -96,6 +99,11 @@ public class ListTripAdapter extends ArrayAdapter<Trip> {
 		}
 		if (mData.get(position) != null) {
 			holder.textView.setText(mData.get(position).getTripName());
+			if(ParseUser.getCurrentUser().getObjectId().equals(mData.get(position).getOrganizerId())){
+				holder.roleView.setImageResource(R.drawable.organizer);
+			} else {
+				holder.roleView.setImageResource(R.drawable.mates);
+			}
 		} else {
 			holder.textView.setText(mDataHeader.get(position));
 		}
@@ -105,6 +113,7 @@ public class ListTripAdapter extends ArrayAdapter<Trip> {
 
 	public static class ViewHolder {
 		public TextView textView;
+		public ImageView roleView;
 	}
 
 }
